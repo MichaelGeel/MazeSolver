aaf7d05564e365549019de70d4be18ee48e7f80e
MichaelGeel <mnrmichaelgeel@gmail.com>
2025-03-02 21:05:01 +0200

    Lesson 4 Completed
    This lesson introduced a Line class and a Point class, used in conjunction with the Window class in order to draw lines on the canvas.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from window import Window
+from window import Window, Point, Line
 
 win = Window(800, 600)
+
+p1 = Point(0, 0)
+p2 = Point(100, 200)
+l1 = Line(p1, p2)
+win.draw_line(l1, "red")
+
+
 win.wait_for_close()
--- window.py
+++ window.py
@@ -1,14 +1,29 @@
 from tkinter import Tk, BOTH, Canvas
 
-## Not creating a window using Tk here, creating a Window class that inherits FROM Tk.
+
+class Point():
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, point_1: Point, point_2: Point):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+
+    def draw(self, canvas: Canvas, color: str):
+        canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=color, width=2)
+
 
 class Window():
-    def __init__(self, width, height):
+    def __init__(self, w: int, h: int):
         self.__root = Tk()
-        self.__root.config(width=width, height=height)
+        self.__root.config(width=w, height=h)
         self.__root.title("MazeSolver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(master=self.__root, background="blue")
+        self.canvas = Canvas(master=self.__root, background="blue", width=w, height=h)
         self.canvas.pack()
         self.running = False
 
@@ -17,10 +32,18 @@ class Window():
         self.__root.update_idletasks()
         self.__root.update()
 
+
     def wait_for_close(self):
         self.running = True
         while self.running:
             self.redraw()
 
+
     def close(self):
         self.running = False
+
+
+    def draw_line(self, line: Line, color: str):
+        # if isinstance(line, Line):
+        line.draw(self.canvas, color)
+        # raise Exception(f"Expected object of class 'Line', got object of type {type(line)}")
